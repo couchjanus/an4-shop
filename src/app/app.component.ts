2f600d2f10567c030853ca998f26d1c2f875397a
Janus Nicon <janusnic@gmail.com>
2017-09-07 11:06:59 +0300

    added unit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Peculiar Shop';
+  title = 'Peculiar Shopaholic';
+  // title: string; // Класс содержит поле свойства title с типом string, запол­няется в конструкторе
+
+  // constructor() { // Функция-конструктор вы­зывается автоматически при создании экземпляра класса
+  //  this.title = 'Peculiar Shopaholic';
+  // }
+
 }
